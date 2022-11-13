e2e47c5af7ff53867461d58cd9e1089eca79ed9f
ninja-96 <okachalov@icloud.com>
2022-11-13 14:28:03 +0300

    do not build python

--- libtorch/libtorch-cuda-build.sh
+++ libtorch/libtorch-cuda-build.sh
@@ -7,15 +7,18 @@ fi
 
 git clone -b $1 --recurse-submodule --depth 1 https://github.com/pytorch/pytorch.git;
 
-mkdir libtorch-$1-$(arch)-build;
-cd libtorch-$1-$(arch)-build;
+mkdir libtorch-$1-$(arch)-cuda-build;
+cd libtorch-$1-$(arch)-cuda-build;
 
 cmake -DUSE_OPENCL:BOOL=OFF \
     -DUSE_CUDA:BOOL=ON \
+    -DUSE_VULKAN:BOOL=OFF \
+    -DBUILD_PYTHON:BOOL=OFF \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DPYTHON_EXECUTABLE:PATH=`which python3` \
-    -DCMAKE_INSTALL_PREFIX:PATH=../libtorch-$1-$(arch) \
+    -DCMAKE_INSTALL_PREFIX:PATH=../libtorch-$1-$(arch)-cuda \
     ../pytorch;
 
 make -j$(nproc);
+make install;
--- libtorch/libtorch-opencl-build.sh
+++ libtorch/libtorch-opencl-build.sh
@@ -7,15 +7,18 @@ fi
 
 git clone -b $1 --recurse-submodule --depth 1 https://github.com/pytorch/pytorch.git;
 
-mkdir libtorch-$1-$(arch)-build;
-cd libtorch-$1-$(arch)-build;
+mkdir libtorch-$1-$(arch)-opencl-build;
+cd libtorch-$1-$(arch)-opencl-build;
 
 cmake -DUSE_OPENCL:BOOL=ON \
     -DUSE_CUDA:BOOL=OFF \
+    -DUSE_VULKAN:BOOL=OFF \
+    -DBUILD_PYTHON:BOOL=OFF \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DPYTHON_EXECUTABLE:PATH=`which python3` \
-    -DCMAKE_INSTALL_PREFIX:PATH=../libtorch-$1-$(arch) \
+    -DCMAKE_INSTALL_PREFIX:PATH=../libtorch-$1-$(arch)-opencl \
     ../pytorch;
 
 make -j$(nproc);
+make install;
