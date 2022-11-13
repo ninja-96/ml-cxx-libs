7bff85d21f55ede6f63df6fcbef973157f6a64ac
ninja-96 <okachalov@icloud.com>
2022-11-13 14:28:16 +0300

    update deps for opencl support

--- libtorch/README.md
+++ libtorch/README.md
@@ -1,1 +1,4 @@
+Only native compile (crosscompile sh not not created).
+Recommended to compile in Docker, Ubuntu 20.04.
+
 Full tutorial: https://github.com/pytorch/pytorch/blob/master/docs/libtorch.rst
--- libtorch/libtorch-deb-deps.sh
+++ libtorch/libtorch-deb-deps.sh
@@ -2,4 +2,13 @@
 
 apt update -y;
 apt upgrade -y;
-apt install build-essential git cmake python3-dev python3-yaml python3-typing-extensions python3-setuptools -y;
+apt install build-essential \
+    git \
+    cmake \
+    ocl-icd-opencl-dev \
+    ocl-icd-dev \
+    python3-dev \
+    python3-yaml \
+    python3-typing-extensions \
+    python3-setuptools \
+    -y;
